2ce321757f86aab8beaab5c2e4865c78680914bc
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-05-02 21:23:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,5 +2,16 @@ class Triangle
 attr_accessor :a, :b, :c 
 
 def initialize(a,b,c)
+@a = a 
+@b = b 
+@c = c 
+end
 
+def kind 
+  if a == b && b == c 
+    :isosceles 
+  elsif 
+  a == b
+
+end
 end
